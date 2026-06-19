8ccef127c41aa99cad335b392d078ad34aadbe80
TomKley345 <tk345@gmx.de>
2026-06-19 08:39:17 +0200

    Update echo message for building Docker image

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo 'building the Doker Image'
+    echo 'jenkins-shared-library: building the Doker Image'
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         sh 'docker build -t tomkley/demo-app:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
