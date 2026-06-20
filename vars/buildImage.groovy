2d58bed97eb91ff79e49931bc09d511895e07b29
Tom Kley <tk345@gmx.de>
2026-06-20 22:24:41 +0200

    bUMP UP APP VERSION TO 2.1

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,8 +3,8 @@
 def call() {
     echo 'building the Doker Image'
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh 'docker build -t tomkley/demo-app:jma-2.0 .'
+        sh 'docker build -t tomkley/demo-app:jma-2.1 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push tomkley/demo-app:jma-2.0'
+        sh 'docker push tomkley/demo-app:jma-2.1'
     }
 }
