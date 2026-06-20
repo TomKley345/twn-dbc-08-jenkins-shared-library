49698e625003dd016e9af94f2b126552dbae876c
TomKley345 <tk345@gmx.de>
2026-06-20 22:43:20 +0200

    Fix typo in Docker image build message

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo 'jenkins-shared-library: building the Doker Image'
+    echo 'jenkins-shared-library: building the Docker Image'
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         sh 'docker build -t tomkley/demo-app:jma-2.1 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
