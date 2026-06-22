0510a6fcb2ebac85a67221a801f02c42d6e3b237
TomKley345 <tk345@gmx.de>
2026-06-22 20:54:30 +0200

    Update Docker image tag from jma-2.3 to jma-2.2

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,8 +4,8 @@ def call() {
     echo 'jenkins-shared-library: building the Docker Image'
 
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh 'docker build -t tomkley/demo-app:jma-2.3 .'
+        sh 'docker build -t tomkley/demo-app:jma-2.2 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push tomkley/demo-app:jma-2.3'
+        sh 'docker push tomkley/demo-app:jma-2.2'
     }
 }
