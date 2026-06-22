80d8fba19af145ca0afd7e606e33800a5af4a7c3
TomKley345 <tk345@gmx.de>
2026-06-22 16:59:33 +0200

    Remove Docker commands from buildImage.groovy
    Removed Docker build and push commands from buildImage.groovy.

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -10,15 +10,4 @@ def call() {
         // Sucht den absoluten Pfad zur Docker-Executable
         sh 'which docker || echo "Docker wurde im PATH nicht gefunden"'
     }
-
-    node {
-        withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-            sh 'echo $USER'
-            sh 'docker build -t tomkley/demo-app:jma-2.1 .'
-            // no need to use full path /usr/bin/docker instead of plain 'docker'
-            // sh '/usr/bin/docker build -t tomkley/demo-app:jma-2.1 .'
-            sh 'echo $PASS | docker login -u $USER --password-stdin'
-            sh 'docker push tomkley/demo-app:jma-2.1'
-        }
-    }
 }
