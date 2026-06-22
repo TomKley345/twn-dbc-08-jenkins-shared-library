7a4d325fa96922375edeced7c5a9f4aafe6f283b
TomKley345 <tk345@gmx.de>
2026-06-22 21:05:17 +0200

    Parameterize Docker image name in buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo 'jenkins-shared-library: building the Docker Image'
 
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh 'docker build -t tomkley/demo-app:jma-2.2 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push tomkley/demo-app:jma-2.2'
+        sh "docker push $imageName"
     }
 }
