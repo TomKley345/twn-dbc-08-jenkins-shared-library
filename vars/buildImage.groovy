f7e821cfc41f0cf3afe0fc9b417d5d8d794b774e
TomKley345 <tk345@gmx.de>
2026-06-22 09:03:14 +0200

    Simplify Docker build command in buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,9 +4,9 @@ def call() {
     echo 'jenkins-shared-library: building the Docker Image'
     node {
         withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-            // sh 'docker build -t tomkley/demo-app:jma-2.1 .'
-            // use full path /usr/bin/docker instead of plain 'docker'
-            sh '/usr/bin/docker build -t tomkley/demo-app:jma-2.1 .'
+            sh 'docker build -t tomkley/demo-app:jma-2.1 .'
+            // no need to use full path /usr/bin/docker instead of plain 'docker'
+            // sh '/usr/bin/docker build -t tomkley/demo-app:jma-2.1 .'
             sh 'echo $PASS | docker login -u $USER --password-stdin'
             sh 'docker push tomkley/demo-app:jma-2.1'
         }
