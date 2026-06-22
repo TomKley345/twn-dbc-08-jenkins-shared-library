f1dbc1d92c35bb48b9723ef3c7c14dab008e13ca
TomKley345 <tk345@gmx.de>
2026-06-22 09:06:07 +0200

    Print Docker username before image build
    Added echo command to print Docker username before building the image.

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,6 +4,7 @@ def call() {
     echo 'jenkins-shared-library: building the Docker Image'
     node {
         withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+            sh 'echo $USER'
             sh 'docker build -t tomkley/demo-app:jma-2.1 .'
             // no need to use full path /usr/bin/docker instead of plain 'docker'
             // sh '/usr/bin/docker build -t tomkley/demo-app:jma-2.1 .'
