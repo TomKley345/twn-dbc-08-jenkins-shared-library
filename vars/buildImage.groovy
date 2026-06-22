83fd5078504491dbf035b30af63e8f15f846c6e3
TomKley345 <tk345@gmx.de>
2026-06-22 09:00:51 +0200

    Refactor buildImage.groovy to use node block

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,11 +2,13 @@
 
 def call() {
     echo 'jenkins-shared-library: building the Docker Image'
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        // sh 'docker build -t tomkley/demo-app:jma-2.1 .'
-        // use full path /usr/bin/docker instead of plain 'docker'
-        sh '/usr/bin/docker build -t tomkley/demo-app:jma-2.1 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push tomkley/demo-app:jma-2.1'
+    node {
+        withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+            // sh 'docker build -t tomkley/demo-app:jma-2.1 .'
+            // use full path /usr/bin/docker instead of plain 'docker'
+            sh '/usr/bin/docker build -t tomkley/demo-app:jma-2.1 .'
+            sh 'echo $PASS | docker login -u $USER --password-stdin'
+            sh 'docker push tomkley/demo-app:jma-2.1'
+        }
     }
 }
